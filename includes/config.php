6cd5f4570e3609faa489906793b5593ca2086a81
j.gardezi <jgardezi@gmail.com>
2012-02-28 15:19:53 +1100

    This is the first commit and project setup.

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database Constants
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER')   ? null : define("DB_USER", "root");
+defined('DB_PASS')   ? null : define("DB_PASS", "");
+defined('DB_NAME_SRC')   ? null : define("DB_NAME", "rgw");
+defined('DB_NAME_TARG_PROFILE')   ? null : define("DB_NAME", "profiles");
+
+?>
